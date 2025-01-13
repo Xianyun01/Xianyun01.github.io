eaa136ae718e598a80455a0ae0bd88f81db09d95
nZero <damun902@gmail.com>
2025-01-13 19:20:22 +0900

    ads

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 /* typing animation */
 var typed = new Typed(".typing",{
-    strings:["Programmer", "", "King", ""],
+    strings:["Programmer", "", "King", "", "Dreamer" , ""],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
 })
+/* ============================================== Aside ============================================== */
+const nav = document.querySelector(".nav")
